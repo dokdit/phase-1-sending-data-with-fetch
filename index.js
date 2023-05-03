c6520204075e4550aa2457c6023ab529a8c08bb9
AK <akueiminyang@cmail.carleton.ca>
2023-05-03 17:44:38 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(userName,userEmail){
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({name:userName,email:userEmail})
+    })
+    .then(res=> res.json())
+    .then(data => {
+        const userID = document.createElement("p");
+        userID.innerHTML = `${data.id}`;
+        document.body.appendChild(userID);
+    })
+    .catch(e => {
+        const errorMessage = document.createElement("p");
+        errorMessage.innerHTML = `Error: ${e}`;
+        document.body.appendChild(errorMessage);
+    });
+}
